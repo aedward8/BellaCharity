9ed03662d73818a6eb03ae8931c1ac30b69b3d50
Abby <Abigail.Edwards317@gmail.com>
2021-04-28 17:32:00 -0700

    Create sample form with results sent to google sheets working

--- app/src/apiClient.js
+++ app/src/apiClient.js
@@ -13,3 +13,25 @@ export const addTask = async (name) => {
   });
   return response.json();
 };
+
+// TEST API
+export const addEntry = async (full_name, email, age, message) => {
+  try {
+    const body = { data: { full_name, email, age, message } };
+    const response = await fetch(
+      "https://api.apispreadsheets.com/data/11586/",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body), //  converts JS object to JSON string
+      },
+    );
+    console.log(response);
+    //converts from JSON to JS
+    return response.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
