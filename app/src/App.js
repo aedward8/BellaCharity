38b1e48f0599a735b76627243277fc0c0ab5db35
Abigail Edwards <63216164+aedward8@users.noreply.github.com>
2021-05-03 09:46:05 -0700

    Revert "Ap isheet"

--- app/src/App.js
+++ app/src/App.js
@@ -16,7 +16,6 @@ const App = () => {
       <h1>test</h1>
       <TaskList tasks={tasks} />
       <AddTask loadTasks={loadTasks} />
-      <Form />
     </main>
   );
 };
@@ -51,91 +50,6 @@ const AddTask = ({ loadTasks }) => {
       </label>
       <button disabled={!canAdd}>Add</button>
     </form>
-  );
-};
-
-const Form = () => {
-  const [full_name, setFullName] = React.useState("");
-  const [email, setEmail] = React.useState("");
-  const [age, setAge] = React.useState("");
-  const [message, setMessage] = React.useState("");
-
-  const subForm = async () => {
-    //e.preventDefault();
-    let res = await apiClient.addEntry(full_name, email, age, message);
-    console.log(res);
-    setFullName("");
-    setEmail("");
-    setAge("");
-    setMessage("");
-  };
-
-  return (
-    <>
-      <br />
-      <br />
-      <h2>Test API</h2>
-      <form id="myForm">
-        <label>
-          Full Name
-          <br />
-          <input
-            name="full_name"
-            value={full_name}
-            onChange={(e) => {
-              setFullName(e.target.value);
-            }}
-          />
-        </label>
-        <br />
-        <label>
-          Email
-          <br />
-          <input
-            name="email"
-            value={email}
-            onChange={(e) => {
-              setEmail(e.target.value);
-            }}
-          />
-        </label>
-        <br />
-        <label>
-          Age
-          <br />
-          <input
-            type="radio"
-            id="age"
-            name="age"
-            value="18-35"
-            onChange={(e) => setAge(e.target.value)}
-          />{" "}
-          18-35
-          <br />
-          <input
-            type="radio"
-            id="age"
-            name="age"
-            value="35+"
-            onChange={(e) => setAge(e.target.value)}
-          />{" "}
-          35+
-        </label>
-        <br />
-        <label>
-          Message
-          <br />
-          <textarea
-            name="message"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-          ></textarea>
-        </label>
-        <br />
-        <br />
-      </form>
-      <button onClick={() => subForm()}>Submit</button>
-    </>
   );
 };
 
