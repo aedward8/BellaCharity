770f5589d9b30e9ceb8ea03ab7796149ed06d89b
George Song <george@monozuku.com>
2021-05-05 11:53:07 -0700

    refactor: use compositional Formik components

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-import { useFormik } from "formik";
+import { Formik, Form, Field as FormikField, ErrorMessage } from "formik";
 import * as Yup from "yup";
 
 import * as apiClient from "./apiClient";
@@ -142,77 +142,49 @@ const ApiForm = () => {
 };
 
 //Formik
+const initialValues = {
+  firstName: "",
+  lastName: "",
+  email: "",
+};
 
-const SignupForm = () => {
-  const formik = useFormik({
-    initialValues: {
-      firstName: "",
-      lastName: "",
-      email: "",
-    },
-    validationSchema: Yup.object({
-      firstName: Yup.string()
-        .max(15, "Must be 15 characters or less")
-        .required("Required"),
-      lastName: Yup.string()
-        .max(20, "Must be 20 characters or less")
-        .required("Required"),
-      email: Yup.string().email("Invalid email address").required("Required"),
-    }),
-    onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
-    },
-  });
+const validationSchema = Yup.object({
+  firstName: Yup.string()
+    .max(15, "Must be 15 characters or less")
+    .required("Required"),
+  lastName: Yup.string()
+    .max(20, "Must be 20 characters or less")
+    .required("Required"),
+  email: Yup.string().email("Invalid email address").required("Required"),
+});
 
-  return (
-    <>
-      <h1>Formik Form</h1>
-      <form onSubmit={formik.handleSubmit}>
-        {/* Pass the useFormik() hook initial form values and a submit function that will
-      be called when the form is submitted */}
-        <label htmlFor="firstName">First Name</label>
-        <input
-          id="firstName"
-          name="firstName"
-          type="text"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.firstName}
-        />
-        {formik.touched.firstName && formik.errors.firstName ? (
-          <div>{formik.errors.firstName}</div>
-        ) : null}
+const onSubmit = (values) => {
+  alert(JSON.stringify(values, null, 2));
+};
 
-        <label htmlFor="lastName">Last Name</label>
-        <input
-          id="lastName"
-          name="lastName"
-          type="text"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.lastName}
-        />
-        {formik.touched.lastName && formik.errors.lastName ? (
-          <div>{formik.errors.lastName}</div>
-        ) : null}
-
-        <label htmlFor="email">Email Address</label>
-        <input
-          id="email"
-          name="email"
-          type="email"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.email}
-        />
-        {formik.touched.email && formik.errors.email ? (
-          <div>{formik.errors.email}</div>
-        ) : null}
+const SignupForm = () => (
+  <>
+    <h1>Formik Form</h1>
+    <Formik {...{ initialValues, validationSchema, onSubmit }}>
+      <Form>
+        <Field label="First Name" name="firstName" />
+        <Field label="Last Name" name="lastName" />
+        <Field label="Email" name="email" type="email" />
         <br />
         <button type="submit">Submit</button>
-      </form>
-    </>
-  );
-};
+      </Form>
+    </Formik>
+  </>
+);
+
+const Field = ({ label, name, type = "text", ...props }) => (
+  <>
+    <label>
+      {label}
+      <FormikField {...{ name, type }} {...props} />
+    </label>
+    <ErrorMessage {...{ name }} component="div" />
+  </>
+);
 
 export default App;
