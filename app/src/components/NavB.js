e2e995446772e23a35b88a55db111260be241167
Abby <Abigail.Edwards317@gmail.com>
2021-05-15 17:58:25 -0700

    added logo to the nav bar, next step to size it properly

--- app/src/components/NavB.js
+++ app/src/components/NavB.js
@@ -1,10 +1,12 @@
 import * as React from "react";
 
-// import "bootstrap/dist/css/bootstrap.min.css";
+import InstagramIcon from "@material-ui/icons/Instagram";
 // import { Link } from "@reach/router";
 import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import Navbar from "react-bootstrap/Navbar";
+
+import Logo from "../photos/bellaCharityLogo.png";
 
 const NavB = () => {
   return (
@@ -13,12 +15,14 @@ const NavB = () => {
     // <Link to="/adopt">Adoptions</Link>
     // <Link to="/signup-form">Volunteer Form</Link> */}
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Navbar.Brand href="/">
+        <img src={Logo} alt="logo image - paw in hand" />{" "}
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/adopt">Adoption Page</Nav.Link>
           <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
             <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
             <NavDropdown.Item href="#action/3.2">
@@ -32,7 +36,9 @@ const NavB = () => {
           </NavDropdown>
         </Nav>
         <Nav>
-          <Nav.Link href="#deets">More deets</Nav.Link>
+          <Nav.Link href="#deets">
+            <InstagramIcon />{" "}
+          </Nav.Link>
           <Nav.Link eventKey={2} href="#memes">
             Dank memes
           </Nav.Link>
