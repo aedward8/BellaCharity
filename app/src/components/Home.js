ede1e3f065902054d10523687e2dd1889564d185
Abby <Abigail.Edwards317@gmail.com>
2021-05-20 13:25:37 -0700

    removed tasks components from template repo

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -2,25 +2,15 @@ import * as React from "react";
 
 import * as apiClient from "../apiClient";
 
-import AddTask from "./AddTask";
 import Form from "./Form";
-import TaskList from "./TaskList";
 
 const Home = () => {
-  const [tasks, setTasks] = React.useState([]);
-  //testing
-  const loadTasks = async () => setTasks(await apiClient.getTasks());
-
-  React.useEffect(() => {
-    loadTasks();
-  }, []);
+  React.useEffect(() => {}, []);
 
   return (
     <div>
       This is Home
       <h1>test</h1>
-      <TaskList tasks={tasks} />
-      <AddTask loadTasks={loadTasks} />
       <Form />
     </div>
   );
